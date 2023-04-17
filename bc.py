83e9545bf113f0df4461c0c04b3427dc4d6db376
Atishay Jain <61509695+atishay2305-hub@users.noreply.github.com>
2023-04-17 01:10:39 -0400

    Update bc.py

--- bc.py
+++ bc.py
@@ -9,47 +9,66 @@ def isfloat(num):
     except ValueError:
         return False
 
-def is_valid_expression(string, operators, variables, values):
-    pattern = r'\s*([+\-*/%^=<>!]=|[+\-*/%^&|<>]|&&|\|\||[()\[\]]|\b[a-zA-Z_]\w*\b)\s*'
-    tokens = re.findall(pattern, string)
-    for i, token in enumerate(tokens):
-        if token in operators:
-            if token in ('++', '--'):
-                if i - 1 >= 0 and i + 1 < len(tokens):
-                    if tokens[i-1] == '' and tokens[i+1] not in operators and not isfloat(tokens[i+1]):
-                        if tokens[i+1] not in variables:
-                            variables.append(tokens[i+1])
-                            values.append(0.0)
-                    elif tokens[i-1] not in operators and tokens[i+1] == '' and not isfloat(tokens[i-1]):
-                        if tokens[i-1] not in variables:
-                            variables.append(tokens[i-1])
-                            values.append(0.0)
-                    else:
-                        return False
-            elif token == '-' or token == '!':
-                if i + 1 < len(tokens) and tokens[i+1] not in operators:
-                    if not isfloat(tokens[i+1]) and tokens[i+1] not in variables:
-                        variables.append(tokens[i+1])
+def is_valid_expression(string,operators,variables,values):
+    l=re.split('(\+\+|--|\^|%|/|\*|\+|-|==|!=|>=|<=|>|<|&&|\|\||!)',string)
+    i=0
+    for i in range(0,len(l)):
+        l[i]=l[i].strip()
+    i=0
+    while(i<len(l)):
+        if(l[i] in operators):
+            if(l[i] in ['++','--']):
+                if(i-1>=0 and i+1<len(l) and l[i-1]=='' and l[i+1]!='' and not isfloat(l[i+1])):
+                    if(l[i+1] not in variables):
+                        variables.append(l[i+1])
+                        values.append(0.0)
+                elif(i-1>=0 and i+1<len(l) and l[i+1]=='' and l[i-1]!='' and not isfloat(l[i-1])):
+                    if(l[i-1] not in variables):
+                        variables.append(l[i-1])
                         values.append(0.0)
                 else:
                     return False
+                i+=2
+            elif(l[i]!='-' and l[i]!='!'):
+                if(i-1>=0 and i+1<len(l) and l[i-1]!='' and l[i+1]!='' and l[i+1] not in operators and l[i-1] not in operators):
+                    if(not isfloat(l[i+1]) and l[i+1] not in variables):
+                        variables.append(l[i+1])
+                        values.append(0.0)
+                    if(not isfloat(l[i-1]) and l[i-1] not in variables):
+                        variables.append(l[i-1])
+                        values.append(0.0)
+                elif(i+2<len(l) and l[i+1]=='' and (l[i+2]=='-' or l[i+2]=='!')):
+                    if(not isfloat(l[i-1]) and l[i-1] not in variables):
+                        variables.append(l[i-1])
+                        values.append(0.0)
+                else:
+                    return False
+                i+=2
+            elif(l[i]=='!' ):
+                if(i-1>=0 and i+1<len(l) and l[i-1]=='' and l[i+1] not in operators):
+                    if(not isfloat(l[i+1]) and l[i+1] not in variables and l[i+1]!=''):
+                        variables.append(l[i+1])
+                        values.append(0.0)
+                    i+=2
+                else:
+                    return False
             else:
-                if i - 1 >= 0 and i + 1 < len(tokens):
-                    if tokens[i-1] not in operators and tokens[i+1] not in operators:
-                        if not isfloat(tokens[i-1]) and tokens[i-1] not in variables:
-                            variables.append(tokens[i-1])
-                            values.append(0.0)
-                        if not isfloat(tokens[i+1]) and tokens[i+1] not in variables:
-                            variables.append(tokens[i+1])
-                            values.append(0.0)
-                    else:
-                        return False
+                if(i-1>=0 and i+1<len(l) and l[i+1] not in operators and l[i-1] not in operators and l[i+1]!=''):
+                    if(not isfloat(l[i+1]) and l[i+1] not in variables and l[i+1]!=''):
+                        variables.append(l[i+1])
+                        values.append(0.0)
+                    if(not isfloat(l[i-1]) and l[i-1] not in variables and l[i-1]!=''):
+                        variables.append(l[i-1])
+                        values.append(0.0)
+                else:
+                    return False
+                i+=2
         else:
-            if not isfloat(token) and token not in variables:
-                variables.append(token)
+            if(not isfloat(l[i]) and l[i] not in variables):
+                variables.append(l[i])
                 values.append(0.0)
+            i+=1
     return True
-
 def is_valid_braces(string,operators,variables,values):
     res = string
     while('(' in res):
@@ -264,14 +283,13 @@ def parse_braces(string,operators,variables,values):
             return evl,flag
         res=res[:start]+str(evl)+res[end+1:]
     return parse_low(res,operators,variables,values)
-
 def parse_statement(inputLines,operators,variables,values):
     lines=inputLines.split("\n")
     flag=True
     for string in lines:
         flag=is_valid_statement(string,operators,variables,values)
         if(flag==False or 'print' in variables):
-            # print('parse error')
+            print('parse error')
             return
     for string in lines:
         string=string.strip()
